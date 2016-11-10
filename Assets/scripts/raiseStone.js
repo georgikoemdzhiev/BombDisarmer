a646bb6371524b2b9d0e7dc10ba9d18c84b8f259
boggogo <koemdjiev@gmail.com>
2016-11-10 17:01:44 +0000

    Stones now rise and fall down on collision

--- Assets/scripts/raiseStone.js
+++ Assets/scripts/raiseStone.js
@@ -1,9 +1,34 @@
 ﻿#pragma strict
 
-function Start () {
+private var isPlayerCollided = null;
+private var distance = 8;
+private var originalYValue = 0;
 
+function Start () {
+	originalYValue = transform.position.y;
+	Debug.Log ("Original Y value: " + transform.position.y);
 }
 
 function Update () {
+	if(isPlayerCollided != null){
+		if(isPlayerCollided && transform.position.y < 20){
+			transform.position.y += 8 * Time.deltaTime;
+			Debug.Log ("Y: " + transform.position.y);
+		}
 
+		if(!isPlayerCollided && transform.position.y > originalYValue){
+			transform.position.y -= 8 * Time.deltaTime;
+		}
+
+	}
+}
+
+function OnTriggerStay (Ent : Collider) {
+    Debug.Log ("Collision detected! OnTriggerEnter");
+    isPlayerCollided = true;
+}
+
+function OnTriggerExit (Ent: Collider) {
+	Debug.Log ("Collision detected! OnTriggerExit");
+	isPlayerCollided = false;
 }

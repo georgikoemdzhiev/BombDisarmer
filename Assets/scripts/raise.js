195f2ddf260b67febea2cd8ba53b2f937e89d2a6
boggogo <koemdjiev@gmail.com>
2016-11-10 22:52:08 +0000

    Code clean up + collider size adjustments

--- Assets/scripts/raise.js
+++ Assets/scripts/raise.js
@@ -1,13 +1,10 @@
 ﻿#pragma strict
 
-private var isPlayerCollided = null;
 private var moveSpeed = 8;
 private var distance = 5;
-private var originalYValue = 0;
 
 function Start () {
-	originalYValue = transform.position.y;
-	Debug.Log ("Original Y value: " + transform.position.y);
+
 }
 
 function Update () {
@@ -16,12 +13,12 @@ function Update () {
 
 function OnTriggerEnter (Ent : Collider) {
     Debug.Log ("Collision detected! OnTriggerEnter");
-
+    // Move the object up when the player enters the target collider 
     transform.position = Vector3.Lerp (transform.position, transform.position + Vector3(0,distance,0), moveSpeed);
 }
 
 function OnTriggerExit (Ent: Collider) {
 	Debug.Log ("Collision detected! OnTriggerExit");
-	//isPlayerCollided = false;
+	// Move the object down when the player exits the target collider
 	transform.position = Vector3.Lerp (transform.position, transform.position - Vector3(0,distance,0), moveSpeed);
 }

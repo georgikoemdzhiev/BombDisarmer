fb8d2f0e2f16d08bd7384338a25e48058b77d94b
boggogo <koemdjiev@gmail.com>
2016-11-10 23:33:57 +0000

    BDP works
    Bomb disarming plan now is placed randomly in the scece.

---
+++ Assets/scripts/GenerateRandomPlan.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var targetListOfObjects : GameObject[];
+public var bombDisarmingPlan : GameObject;
+
+function Start () {
+	var randomTarget = Random.Range(0,targetListOfObjects.Length);
+	bombDisarmingPlan.transform.position.x = 
+	targetListOfObjects[randomTarget].transform.position.x;
+
+	bombDisarmingPlan.transform.position.z = 
+	targetListOfObjects[randomTarget].transform.position.z;
+
+	Debug.Log ("Random targer index: " + randomTarget);
+}
+
+function Update () {
+
+}

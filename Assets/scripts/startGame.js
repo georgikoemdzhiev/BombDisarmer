a4811be8ebc8d7a4640e7ae7ecbc7d3663ea0d16
boggogo <koemdjiev@gmail.com>
2016-11-24 16:36:47 +0000

    Timer starts when the player plresses the button

--- Assets/scripts/startGame.js
+++ Assets/scripts/startGame.js
@@ -1,10 +1,14 @@
 ﻿#pragma strict
 var player: Transform;
 var startGameBtn: Transform;
-
+ 
 private var generateRanPlanScript: GenerateRandomPlan;
 private var playerCloseToButton = false;
 private var originalButtonZvalue;
+private var distanceAllowed = 1.7;
+private var isGameStarted: boolean = false;
+private var timer: float = 10.0;
+
 function Start () {
 	originalButtonZvalue = startGameBtn.position.z;
 	generateRanPlanScript = GetComponent("GenerateRandomPlan");
@@ -13,11 +17,12 @@ function Start () {
 function Update () {
 	
 	var fwd: Vector3 = startGameBtn.TransformDirection(Vector3.forward);
-	if (Physics.Raycast(player.position, fwd, 1.5)){
+	if (Physics.Raycast(player.position, fwd, distanceAllowed)){
 		//print("There is something in front of the object!");
 		//Debug.Log( "Player is close to the button");
 		if (Input.GetMouseButtonDown(0)) {
 			Debug.Log( "Player clicked mouse left button - start the game");
+			isGameStarted = true;
 			startGameBtn.position.z += 0.3;
 			generateRanPlanScript.GenerateRandomPlan();
 		}
@@ -25,7 +30,23 @@ function Update () {
 		if (Input.GetMouseButtonUp(0)) {
 		startGameBtn.position.z = originalButtonZvalue;
 		}
+		
 	}
 	
+		if(isGameStarted) {
+			timer -= Time.deltaTime;
+			if(timer <= 0){
+				//Game Over
+				timer = 0;
+				isGameStarted = false;
+			}
+		}
+	
 }
+
+function OnGUI() {
+	GUI.Box(new Rect(10,10,50,20), "" + timer.ToString("0"));
+}
+
+ 
 

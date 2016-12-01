cb7cc3dbc0840e94dc2afe36433f25f8c0671ab9
boggogo <koemdjiev@gmail.com>
2016-12-01 14:29:02 +0000

    Press left mouse button to continue

--- Assets/scripts/TransferPlayerToNextScene.js
+++ Assets/scripts/TransferPlayerToNextScene.js
@@ -1,22 +1,28 @@
 ﻿#pragma strict
 import UnityEngine.SceneManagement;
+import UnityEngine.UI;
+
 var player: Transform;
+var goToNextSceneText: Text;
 //var gameDisarmingPlan: Transform;
 
 
 private var distanceAllowed = 1.5;
 
 function Start () {
-
+	goToNextSceneText.enabled = false;
 }
 
 function Update () {
 	var distance = Vector3.Distance(transform.position, player.position);
 	//Debug.Log( "Player is close: " + distance.ToString());
 	//Check if the player is close to the bomb and clicks the left mouse button
-	if(distance <= 2 && Input.GetMouseButtonUp(0)){
-		Debug.Log("Load the next leval");
-		SceneManager.LoadScene ("BombDisarmingScene");
+	if(distance <= 2){ 
+	// Show text 'Press the mouse left button in order to go to the bext scene
+	goToNextSceneText.enabled = true;
+		if(Input.GetMouseButtonUp(0)){
+			Debug.Log("Load the next leval");
+			SceneManager.LoadScene ("BombDisarmingScene");
+		}
 	}
-
 }

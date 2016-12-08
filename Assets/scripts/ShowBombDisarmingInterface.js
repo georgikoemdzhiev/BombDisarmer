661030b8179f89181619c081d17a643003441286
boggogo <koemdjiev@gmail.com>
2016-12-08 16:52:06 +0000

    Show bomb disarming interface with key press works

---
+++ Assets/scripts/ShowBombDisarmingInterface.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var PressButtonText: Text;
+var gameDisarmingInterface: Image;
+
+function Start () {
+//	ObjDisable();
+}
+
+function Update () {
+	if (Input.GetKeyDown ("o")){
+		print ("space key was pressed");
+		PressButtonText.enabled = false;
+		gameDisarmingInterface.gameObject.active = true;
+	}
+        
+}
+
+function OnTriggerEnter() {
+	PressButtonText.enabled = true;
+	Debug.Log("OnTriggerEnter");
+}
+
+function OnTriggerExit() {
+	PressButtonText.enabled = false;
+	Debug.Log("OnTriggerExit");
+}

ef34e965c00a483c20e6c2c9aac8cc59f7d0e02c
boggogo <koemdjiev@gmail.com>
2016-12-16 19:59:24 +0000

    Adding code comments

--- Assets/scripts/GenerateRandomPlanLocation.js
+++ Assets/scripts/GenerateRandomPlanLocation.js
@@ -11,6 +11,9 @@ function Update () {
 
 }
 
+/**
+* Method to generate a random positon of the bomb disarming plan
+*/
 function GenerateRandomPlan() {
 	var randomTarget = Random.Range(0,targetListOfObjects.Length);
 	bombDisarmingPlan.transform.position.x = 
